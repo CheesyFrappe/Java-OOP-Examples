072b201fcce0683d1457d3acb630ad496d23ef8d
Emir Balcı <aerangalerind@hotmail.com>
2022-05-12 12:55:10 +0300

    Update readme.java

--- Interface/readme.java
+++ Interface/readme.java
@@ -1,8 +1,8 @@
 /*
     • Write an Interface named Interface1.
 
-    • Interface1 has 3 abstract methods named speak(),walk() and swim().
-    • Write 2 Java classes named Cat and Dog which implement Interface1.
+    • Interface1 has three abstract methods named speak(), walk() and swim().
+    • Write two Java classes named Cat and Dog which implement Interface1.
     • Write a controller class named Main. Create an arraylist with the size of 10 elements consisting of Cat and Dog objects.
 
  */
